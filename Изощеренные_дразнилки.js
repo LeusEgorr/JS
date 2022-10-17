dce4043a025006760f13b1ff911e4393bba80823
Egor Leus <E.Leus@icloud.com>
2022-10-17 13:20:37 +0300

    функция pickRandomWord

--- Изощеренные_дразнилки.js
+++ Изощеренные_дразнилки.js
@@ -1,15 +1,9 @@
-var randomBodyParts=["нога","рука","шея","голова","стопа"];
-var randomAdjectives=["длинная","некрасивая","короткая","страшная","ужасная"];
-var randomAnimalBodyParts=["хвост","шея","копыта","пасть","туловище"];
-var randomAnimals=["бегемота","жирафа","козы","осла","динозавра"];
-//Выбор случайной части тела:
-var randomBodyPart = randomBodyParts[Math.floor(Math.random()*5)]; 
-//Выбор случайного прилагательного:
-var randomAdjective = randomAdjectives[Math.floor(Math.random()*5)];
-//Выбор случайной части тела животного:
-var randomAnimalBodyPart = randomAnimalBodyParts[Math.floor(Math.random()*5)];
-//Выбор случайного животного:
-var randomAnimal = randomAnimals[Math.floor(Math.random()*5)];
-//Соединяем случайные строки в предложение:
-var randomResult= "У тебя "+randomBodyPart+ " ещё более "+randomAdjective+", чем "+randomAnimalBodyPart+" y "+randomAnimal+"."
-randomResult;
+var generateRandomInsult=function (){
+    var randomBodyParts=["нога","рука","шея","голова","стопа"];
+    var randomAdjectives=["длинная","некрасивая","короткая","страшная","ужасная"];
+    var randomAnimalBodyParts=["хвост","шея","копыта","пасть","туловище"];
+    var randomAnimals=["бегемота","жирафа","козы","осла","динозавра"];
+    //Соединяем случайные строки в предложение:
+    var randomResult= "У тебя "+pickRandomWord(randomBodyParts)+ " ещё более "+pickRandomWord(randomAdjectives)+", чем "+pickRandomWord(randomAnimalBodyParts)+" y "+pickRandomWord(randomAnimals)+"."
+    return randomResult;
+    };
